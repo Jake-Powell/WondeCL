4661b5ce5640b8041290b93237e53c4057ddcfd5
Jake-Powell <jake-powell@hotmail.co.uk>
2025-10-20 09:29:50 +0100

    Update yaml, namespace and logo

--- inst/create_logo.R
+++ inst/create_logo.R
@@ -1,7 +1,8 @@
 install.packages("hexSticker")
 library(hexSticker)
 
-sticker(subplot = '/Users/jakepowell/OME/WondePull/inst/cat.png', package="WondePull", p_size=20, s_x=1, s_y=.8, s_width=.7,
+# Path specific to device, may need to change.
+sticker(subplot = '~/R packages/Wonde/WondeCL/inst/cat.png', package="WondeCL", p_size=20, s_x=1, s_y=.8, s_width=.7,
         h_fill="#009BC1", h_color="#10263B",
         filename="inst/figures/imgfile.png")
 usethis::use_logo("inst/figures/imgfile.png")
